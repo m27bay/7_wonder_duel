62a2b59ecce3f77ae35141d01d9f84823813af05
m27bay <mickael.ledenmat@gmail.com>
2022-02-25 11:47:40 +0100

    ajout docstring + changement convention nommage + ajout test compter point victoire

--- test/TestJoueur.py
+++ test/TestJoueur.py
@@ -1,10 +1,17 @@
+"""
+Fichier test de la classe Joueur.
+"""
+
 import unittest
 
-from src.main import Joueur, Carte
+from src.main import Joueur, Carte, Merveille, JetonProgres
 
 
 class TestJoeur(unittest.TestCase):
 	def setUp(self) -> None:
+		"""
+		Initialise un joueur pour la suite des test.
+		"""
 		self.joueur = Joueur("pierre")
 
 	def testConstructeur(self):
@@ -13,41 +20,41 @@ class TestJoeur(unittest.TestCase):
 	def testRessourcesManquantes(self):
 		self.joueur.cartes.append(Carte(None, None, ["ressource bois 2"], None, None, None, None))
 		carte = Carte("carte", None, None, ["ressource bois 2"], None, None, None)
-		self.assertEqual([], self.joueur.coutsManquant(carte))
+		self.assertEqual([], self.joueur.couts_manquants(carte))
 		self.joueur.cartes.clear()
 
 		self.joueur.cartes.append(Carte(None, None, ["ressource bois 2"], None, None, None, None))
 		carte2 = Carte("carte2", None, None, ["ressource bois 1", "ressource verre 2"], None, None, None)
-		self.assertEqual(["ressource verre 2"], self.joueur.coutsManquant(carte2))
+		self.assertEqual(["ressource verre 2"], self.joueur.couts_manquants(carte2))
 		self.joueur.cartes.clear()
 
 		self.joueur.cartes.append(Carte(None, None, ["ressource bois 1"], None, None, None, None))
 		carte3 = Carte("carte3", None, None, ["ressource bois 2", "ressource verre 2"], None, None, None)
-		self.assertEqual(["ressource bois 1", "ressource verre 2"], self.joueur.coutsManquant(carte3))
+		self.assertEqual(["ressource bois 1", "ressource verre 2"], self.joueur.couts_manquants(carte3))
 		self.joueur.cartes.clear()
 
 	def testMonnaiesManquantes(self):
 		self.joueur.monnaie = 4
 		carte = Carte(None, None, None, ["monnaie 4"], None, None, None)
-		self.assertEqual([], self.joueur.coutsManquant(carte))
+		self.assertEqual([], self.joueur.couts_manquants(carte))
 
 		carte = Carte(None, None, None, ["monnaie 5"], None, None, None)
-		self.assertEqual(["monnaie 1"], self.joueur.coutsManquant(carte))
+		self.assertEqual(["monnaie 1"], self.joueur.couts_manquants(carte))
 		self.joueur.monnaie = 0
 
 	def testPossedeCarteChainage(self):
 		self.joueur.cartes.append(Carte("carte", None, None, None, None, None, None))
 
-		self.assertTrue(self.joueur.possedeCarteChainage(Carte("carte2", None, None, None, "carte", None, None)))
-		self.assertFalse(self.joueur.possedeCarteChainage(Carte("carte3", None, None, None, "erreur", None, None)))
+		self.assertTrue(self.joueur.possede_carte_chainage(Carte("carte2", None, None, None, "carte", None, None)))
+		self.assertFalse(self.joueur.possede_carte_chainage(Carte("carte3", None, None, None, "erreur", None, None)))
 
 		self.joueur.cartes.clear()
 
 	def testProductionTypeRessources(self):
 		self.joueur.cartes.append(Carte("carte", None, ["ressource bois 1"], None, None, None, None))
 
-		self.assertEqual("carte", self.joueur.productionTypeRessources("ressource bois 1").nom)
-		self.assertEqual(None, self.joueur.productionTypeRessources("ressource pierre 1"))
+		self.assertEqual("carte", self.joueur.production_type_ressources("ressource bois 1").nom)
+		self.assertEqual(None, self.joueur.production_type_ressources("ressource pierre 1"))
 
 		self.joueur.cartes.clear()
 
@@ -56,34 +63,76 @@ class TestJoeur(unittest.TestCase):
 		      None, "jaune", age=2)
 		self.joueur.cartes.append(douane)
 
-		self.assertEqual(1, self.joueur.possedeReduction("papyrus"))
-		self.assertEqual(0, self.joueur.possedeReduction("pierre"))
+		self.assertEqual(1, self.joueur.possede_carte_reduction("papyrus"))
+		self.assertEqual(0, self.joueur.possede_carte_reduction("pierre"))
 
 		self.joueur.cartes.clear()
 
 	def testPossedeCartesCouleur(self):
 		carte = Carte("chantier", None, ["ressource bois 1"], None, None, "marron", age=1)
 		self.joueur.cartes.append(carte)
-		listeCarteCoul = self.joueur.cartesCouleur("marron")
+		liste_carte_coul = self.joueur.possede_cartes_couleur("marron")
 
-		self.assertEqual([carte], listeCarteCoul)
+		self.assertEqual([carte], liste_carte_coul)
 
 		carte2 = Carte("exploitation", None, ["ressource bois 1"], ["monnaie 1"], None, "marron", age=1)
 		self.joueur.cartes.append(carte2)
-		listeCarteCoul = self.joueur.cartesCouleur("marron")
+		liste_carte_coul = self.joueur.possede_cartes_couleur("marron")
 
-		self.assertEqual([carte, carte2], listeCarteCoul)
+		self.assertEqual([carte, carte2], liste_carte_coul)
 
 		self.joueur.cartes.clear()
 
 	def testPossedePasCartesCouleur(self):
 		carte = Carte("chantier", None, ["ressource bois 1"], None, None, "marron", age=1)
 		self.joueur.cartes.append(carte)
-		listeCarteCoul = self.joueur.cartesCouleur("grise")
+		liste_carte_coul = self.joueur.possede_cartes_couleur("grise")
 
-		self.assertEqual([], listeCarteCoul)
+		self.assertEqual([], liste_carte_coul)
 
 		self.joueur.cartes.clear()
+
+	def testCompterPointVictoireAvecLesCartes(self):
+		self.joueur.cartes.append(Carte("theatre", None, ["point_victoire 3"], None, None, "blue", age=1))
+		self.joueur.cartes.append(Carte("autel", None, ["point_victoire 3"], None, None, "blue", age=1))
+		self.assertEqual(6, self.joueur.compter_point_victoire())
+		self.joueur.cartes.clear()
+
+	def testCompterPointVictoireAvecLesMerveilles(self):
+		self.joueur.merveilles.append(
+			Merveille("circus maximus", None,
+			          ["defausse_carte_adversaire grise", "attaquer 1", "point_victoire 3"],
+			          ["ressource pierre 2", "ressource bois 1", "ressource verre 1"]
+			          )
+		)
+		self.joueur.merveilles.append(
+			Merveille("colosse", None,
+			          ["attaquer 2", "point_victoire 3"],
+			          ["ressource argile 3", "ressource verre 1"]
+			          )
+		)
+		self.assertEqual(6, self.joueur.compter_point_victoire())
+		self.joueur.merveilles.clear()
+
+	def testCompterPointVictoireAvecLesJetons(self):
+		self.joueur.jetons.append(
+			JetonProgres("agriculture", None, ["monnaie 6", "point_victoire 4"])
+		)
+		self.joueur.jetons.append(
+			JetonProgres("mathematiques_custom", None, ["point_victoire 3"]),
+		)
+		self.assertEqual(7, self.joueur.compter_point_victoire())
+		self.joueur.merveilles.clear()
+
+	def testCompterPointVictoireAvecLesJetons2(self):
+		self.joueur.jetons.append(
+			JetonProgres("agriculture", None, ["monnaie 6", "point_victoire 4"])
+		)
+		self.joueur.jetons.append(
+			JetonProgres("mathematiques", None, ["point_victoire_par_jeton 3", "point_victoire 3"]),
+		)
+		self.assertEqual(13, self.joueur.compter_point_victoire())
+		self.joueur.merveilles.clear()
 
 
 if __name__ == '__main__':
