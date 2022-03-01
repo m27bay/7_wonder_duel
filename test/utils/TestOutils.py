1fdd80550903a7ee504976b1e9037a8167b5f9aa
m27bay <mickael.ledenmat@gmail.com>
2022-03-01 19:00:47 +0100

    ajout test test_trouver_ressource_avec_nom et test_demander_ressource_dans_une_liste

--- test/utils/TestOutils.py
+++ test/utils/TestOutils.py
@@ -9,6 +9,8 @@ from src.utils.CarteFille import CarteFille
 
 from src.utils.Outils import trouver_element_avec_nom
 from src.utils.Outils import demander_element_dans_une_liste
+from src.utils.Outils import trouver_ressource_avec_nom
+from src.utils.Outils import demander_ressource_dans_une_liste
 
 
 class TestOutils(unittest.TestCase):
@@ -32,19 +34,75 @@ class TestOutils(unittest.TestCase):
 			CarteFille("merveille4", None, None, None)
 		]
 
-	def testTrouverCarteAvecNom(self):
-		self.assertEqual(self.liste_cartes[4], trouver_element_avec_nom("carte4", self.liste_cartes))
-		self.assertEqual(None, trouver_element_avec_nom("erreur", self.liste_cartes))
+	def test_trouver_carte_avec_nom(self):
+		self.assertEqual(
+			self.liste_cartes[4],
+			trouver_element_avec_nom(
+				"carte4",
+				self.liste_cartes)
+		)
+		
+		self.assertEqual(
+			None,
+			trouver_element_avec_nom(
+				"erreur",
+				self.liste_cartes)
+		)
 
-	def testTrouverMerveilleAvecNom(self):
-		self.assertEqual(self.liste_merveilles[2], trouver_element_avec_nom("merveille2", self.liste_merveilles))
-		self.assertEqual(None, trouver_element_avec_nom("erreur", self.liste_merveilles))
+	def test_trouver_merveille_avec_nom(self):
+		self.assertEqual(
+			self.liste_merveilles[2],
+			trouver_element_avec_nom(
+				"merveille2",
+				self.liste_merveilles)
+		)
+		
+		self.assertEqual(
+			None,
+			trouver_element_avec_nom(
+				"erreur",
+				self.liste_merveilles)
+		)
 
-	def testDemanderCarteDansUneListe(self):
+	def test_demander_carte_dans_une_liste(self):
 		# entree : carte0
-		carte_demandee = demander_element_dans_une_liste("joueur1", "carte_a_enlever", self.liste_cartes)
+		
+		carte_demandee = demander_element_dans_une_liste(
+			"joueur1",
+			"carte_a_enlever",
+			self.liste_cartes
+		)
+		
 		self.assertEqual(self.liste_cartes[0], carte_demandee)
 
+	def test_trouver_ressource_avec_nom(self):
+		self.assertEqual(
+			"ressource bois 1",
+			trouver_ressource_avec_nom(
+				"bois",
+				["ressource pierre 2", "ressource argile 1", "ressource bois 1"]
+			)
+		)
+
+		self.assertEqual(
+			None,
+			trouver_ressource_avec_nom(
+				"erreur",
+				["ressource pierre 2", "ressource argile 1", "ressource bois 1"]
+			)
+		)
+
+	def test_demander_ressource_dans_une_liste(self):
+		# entree : bois
+		
+		nom_ressource, ressource_choisie = demander_ressource_dans_une_liste(
+			"joueur1",
+			["ressource pierre 2", "ressource argile 1", "ressource bois 1"]
+		)
+
+		self.assertEqual("bois", nom_ressource)
+		self.assertEqual("ressource bois 1", ressource_choisie)
+		
 
 if __name__ == '__main__':
 	unittest.main()
