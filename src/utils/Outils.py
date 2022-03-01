d3bcf4a5afad71f785a0fb7dafa794afe8d0d9a9
m27bay <mickael.ledenmat@gmail.com>
2022-03-01 18:59:56 +0100

    ajout fonction trouver_ressource_avec_nom et demander_ressource_dans_une_liste

--- src/utils/Outils.py
+++ src/utils/Outils.py
@@ -5,52 +5,99 @@ Fonction utilisees dans le plateau
 from src.logger.Logger import logger
 
 
-def trouver_element_avec_nom(nom: str, liste: list):
+def trouver_element_avec_nom(nom_element: str, liste: list):
 	"""
-	Trouver un element (carte_a_enlever, merveille) avec son nom dans une liste.
+	Trouver un element (carte, merveille, jeton progres) avec son nom dans une liste.
 
-	:param nom: nom de l element a chercher.
+	:param nom_element: nom de l element a chercher.
 	:param liste: liste des objets ou chercher l element.
 	:return: l element si trouvee, None sinon.
 	"""
 	
-	logger.debug(f"trouver_element_avec_nom avec le nom \'{nom}\' dans la liste : \n {afficher(liste)}")
+	logger.debug(f"trouver_element_avec_nom(\'{nom_element}\'\n{afficher(liste)})")
 	
 	for element in liste:
-		if element.nom == nom:
-			logger.debug(f"\t\'{nom}\' est dans la liste")
+		if element.nom == nom_element:
+			logger.debug(f"\t\'{nom_element}\' est dans la liste")
 			return element
 	
-	logger.debug(f"\t\'{nom}\' n'est pas dans la liste")
+	logger.debug(f"\t\'{nom_element}\' n'est pas dans la liste")
 	return None
 
 
 def demander_element_dans_une_liste(nom_joueur: str, type_element: str, liste_element: list):
 	"""
-	Renvoie l element contenu dans liste_element correspondant au nom renseigne par le nom_joueur.
+	Renvoie l element contenu dans liste_element correspondant au nom donne par le nom_joueur.
 
 	:param nom_joueur: le nom du nom_joueur a qui on demande l element.
 	:param type_element: le type d element de que l on cherche (uniquement pour l'affichage)
-		(carte_a_enlever, merveille, ..).
+		(carte, merveille, jeton progres).
 	:param liste_element: la liste ou l on cherche l element.
 	:return: l element choisi.
 	"""
 	
-	logger.debug(f"[{nom_joueur}] demander_element_dans_une_liste choisit \'{type_element}\' "
-		f"dans la liste : \n{afficher(liste_element)}")
+	logger.debug(f"[{nom_joueur}] demander_element_dans_une_liste(\'{type_element}\'\n{afficher(liste_element)})")
 	
 	while True:
 		print(f"* liste choix possibles *\n{afficher(liste_element)}")
 		nom_element = input(f"[{nom_joueur}] Choix {type_element} ?\n > ")
 		element_choisi = trouver_element_avec_nom(nom_element, liste_element)
+		
 		if element_choisi is None:
-			print(f" * ERREUR * Aucun element ne repond au nom \'{nom_element}\', veuillez recommencer * ERREUR * ")
+			print(f" * ERREUR * Aucun element ne repond au nom \'{nom_element}\', veuillez recommencer")
 			continue
 		else:
 			break
 	
 	logger.debug(f"[{nom_joueur}] a choisit \'{element_choisi.nom}\'")
 	return element_choisi
+
+
+def trouver_ressource_avec_nom(nom_ressource: str, liste: list):
+	"""
+	Trouver une ressource avec son nom dans une liste.
+
+	:param nom_ressource: le nom de la ressource.
+	:param liste: une liste de ressource.
+	:return: la ressource correspondant au nom.
+	"""
+	
+	for ressource in liste:
+		
+		# decoupage
+		ressource_split = ressource.split(" ")
+		
+		if ressource_split[0] == "ressource":
+			if ressource_split[1] == nom_ressource:
+				return ressource
+	
+	return None
+
+
+def demander_ressource_dans_une_liste(nom_joueur: str, liste_element: list):
+	"""
+	Renvoie l element contenu dans liste_element correspondant au nom donne par le nom_joueur.
+
+	:param nom_joueur: le nom du nom_joueur a qui on demande l element.
+	:param liste_element: la liste ou l on cherche la ressource.
+	:return: la ressource choisie.
+	"""
+	
+	logger.debug(f"[{nom_joueur}] demander_ressource_dans_une_liste(\n{afficher(liste_element)})")
+	
+	while True:
+		print(f"* liste choix possibles *\n{liste_element}")
+		nom_ressource = input(f"[{nom_joueur}] Choix ressource ?\n > ")
+		ressource_choisie = trouver_ressource_avec_nom(nom_ressource, liste_element)
+		
+		if ressource_choisie is None:
+			print(f" * ERREUR * Aucune ressource ne repond au nom \'{nom_ressource}\', veuillez recommencer")
+			continue
+		else:
+			break
+	
+	logger.debug(f"[{nom_joueur}] a choisit \'{ressource_choisie}\'")
+	return nom_ressource, ressource_choisie
 
 
 def afficher(liste: list) -> str:
