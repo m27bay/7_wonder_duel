bbfbf0d38caabad8939aba458c15db36033fd0c8
m27bay <mickael.ledenmat@gmail.com>
2022-03-23 12:42:17 +0100

    affichage merveilles

--- src/interface_graphique/src/Fenetre.py
+++ src/interface_graphique/src/Fenetre.py
@@ -1,13 +1,21 @@
 import pygame
 
 from src.interface_graphique.src.Constantes import DOSSIER_IMAGES
+from src.interface_graphique.src.MonSprite import MonSprite
 from src.interface_graphique.src.SpriteCarte import SpriteCarte
+from src.interface_graphique.src.SpriteJetonsProgres import SpriteJetonsProgres
+from src.interface_graphique.src.SpriteMerveille import SpriteMerveille
 from src.utils.Carte import Carte
 from src.utils.CarteFille import CarteFille
+from src.utils.JetonProgres import JetonProgres
 from src.utils.Plateau import Plateau
 
-RATIO_IMAGE = 0.18
-RATIO_PLATEAU = 0.55
+RATIO_IMAGE = 0.16
+RATIO_MERVEILLE = 0.10
+RATIO_JETONS_PROGRES = 0.10
+
+RATIO_PLATEAU = 0.50
+RATION_BANQUE = 0.05
 RATIO_ZOOM = 0.90
 
 class Fenetre:
@@ -24,6 +32,14 @@ class Fenetre:
 		default_sprite_image = SpriteCarte(Carte("academie", None, None, None, None, 3), 0, 0, RATIO_IMAGE)
 		self.default_hauteur_sprite = default_sprite_image.rect.height
 		self.default_largeur_spirte = default_sprite_image.rect.width
+		
+		default_sprite_merveille = SpriteMerveille(CarteFille("piree", None, None), 0, 0, RATIO_MERVEILLE)
+		self.default_hauteur_merveille = default_sprite_merveille.rect.height
+		self.default_largeur_merveille = default_sprite_merveille.rect.width
+		
+		default_sprite_jetons_progres = SpriteJetonsProgres(JetonProgres("agriculture", None), 0, 0, RATIO_JETONS_PROGRES)
+		self.default_hauteur_jetons_progres = default_sprite_jetons_progres.rect.height
+		self.default_largeur_jetons_progres = default_sprite_jetons_progres.rect.width
 		
 		self.cartes_plateau = pygame.sprite.Group()
 		
@@ -51,7 +67,6 @@ class Fenetre:
 		self.sprite_zoomer = None
 		
 		self.espace_entre_carte = self.largeur * 0.005
-		self.__dessiner_carte()
 		
 		image_fond = pygame.image.load(DOSSIER_IMAGES + "fond_jeux.jpg").convert()
 		self.image_fond = pygame.transform.scale(image_fond, (self.largeur, self.hauteur))
@@ -64,17 +79,41 @@ class Fenetre:
 		haut_plat = ration_plat * larg_plat
 		self.image_plateau = pygame.transform.scale(image_plateau, (larg_plat, haut_plat))
 		self.rect_image_plateau = self.image_plateau.get_rect()
+		self.rect_image_plateau.topleft = (
+			(self.largeur / 2 - self.image_plateau.get_width() / 2,
+			2 * self.espace_entre_carte)
+		)
+		
+		image_banque = pygame.image.load(DOSSIER_IMAGES + "banque_icon.png").convert_alpha()
+		larg_banque, haut_banque = image_banque.get_size()
+		ration_banque = haut_banque / larg_banque
+		larg_banque = RATION_BANQUE * self.largeur
+		haut_banque = ration_banque * larg_banque
+		self.image_banque = pygame.transform.scale(image_banque, (larg_banque, haut_banque))
+		self.rect_image_banque = self.image_banque.get_rect()
+		self.rect_image_banque.topleft = (
+			(self.largeur / 2 - self.image_banque.get_width() / 2,
+			self.hauteur - 2 * self.espace_entre_carte - self.image_banque.get_height())
+		)
+		
+		self.__dessiner_carte()
 		
 		self.double_clic = False
 		
 		self.merverille_j1 = pygame.sprite.Group()
 		self.merverille_j2 = pygame.sprite.Group()
+		self.__dessiner_merveille()
+		
+		self.jetons_progres_plateau = pygame.sprite.Group()
+		
+		self.__dessiner_jetons_scientifiques()
 		
 	def __dessiner_carte_age_I(self):
 		origine_cartes = self.largeur / 2 - self.default_largeur_spirte - self.espace_entre_carte / 2
 		
 		haut_gauche_x = origine_cartes
-		haut_gauche_y = self.hauteur * 1 / 3
+		_, haut_gauche_y = self.rect_image_plateau.bottomright
+		haut_gauche_y += self.espace_entre_carte
 		
 		for num_ligne, ligne_cartes in enumerate(self.plateau.cartes_plateau):
 			for num_colone, carte in enumerate(ligne_cartes):
@@ -95,7 +134,8 @@ class Fenetre:
 		origine_cartes = self.largeur / 2 - 3 * self.default_largeur_spirte - self.espace_entre_carte / 2
 		
 		haut_gauche_x = origine_cartes
-		haut_gauche_y = self.hauteur * 1 / 3
+		_, haut_gauche_y = self.rect_image_plateau.bottomright
+		haut_gauche_y += self.espace_entre_carte
 		
 		for num_ligne, ligne_cartes in enumerate(self.plateau.cartes_plateau):
 			for num_colone, carte in enumerate(ligne_cartes):
@@ -116,7 +156,8 @@ class Fenetre:
 		origine_cartes = self.largeur / 2 - self.default_largeur_spirte - self.espace_entre_carte / 2
 		
 		haut_gauche_x = origine_cartes
-		haut_gauche_y = self.hauteur * 1 / 3
+		_, haut_gauche_y = self.rect_image_plateau.bottomright
+		haut_gauche_y += self.espace_entre_carte
 
 		for num_ligne in range(3):
 			ligne_cartes = self.plateau.cartes_plateau[num_ligne]
@@ -175,7 +216,45 @@ class Fenetre:
 			self.__dessiner_carte_age_III()
 			
 	def __dessiner_merveille(self):
-	
+		compteur = 0
+		for merveille in self.plateau.joueur1.merveilles:
+			coord_x, coord_y = self.rect_image_plateau.bottomleft
+			coord_x += self.espace_entre_carte
+			
+			coord_y += compteur * (self.default_largeur_merveille + self.espace_entre_carte)
+			coord_y += compteur * (self.default_hauteur_sprite / 4)
+			
+			merveille_sprite = SpriteMerveille(merveille, coord_x, coord_y, RATIO_MERVEILLE)
+			merveille_sprite.angle = -90
+			self.merverille_j1.add(merveille_sprite)
+			
+			compteur += 1
+		
+		compteur = 0
+		for merveille in self.plateau.joueur2.merveilles:
+			coord_x, coord_y = self.rect_image_plateau.bottomright
+			coord_x -= self.default_hauteur_merveille
+			coord_x -= self.espace_entre_carte
+			
+			coord_y += compteur * (self.default_largeur_merveille + self.espace_entre_carte)
+			coord_y += compteur * (self.default_hauteur_sprite / 4)
+			
+			sprite_merveille = SpriteMerveille(merveille, coord_x, coord_y, RATIO_MERVEILLE)
+			sprite_merveille.angle = 90
+			self.merverille_j2.add(sprite_merveille)
+			
+			compteur += 1
+			
+	def __dessiner_jetons_scientifiques(self):
+		coord_x, coord_y = self.rect_image_plateau.topleft
+		coord_x += self.rect_image_plateau.width / 2
+		coord_x += self.default_largeur_jetons_progres
+		
+		# compteur = 0
+		# for jeton in self.plateau.jetons_progres_plateau:
+		sprite_jetons_progres = SpriteJetonsProgres(self.plateau.jetons_progres_plateau[1], coord_x, coord_y, RATIO_JETONS_PROGRES)
+		self.jetons_progres_plateau.add(sprite_jetons_progres)
+			#compteur += 1
 	
 	def __position_type_carte(self, carte: Carte):
 		if not isinstance(carte, CarteFille):
@@ -196,13 +275,13 @@ class Fenetre:
 			sprite_joueur_qui_joue = self.sprite_j1
 			sprite_carte.angle = -sprite_carte.angle
 			coord_x, _ = self.rect_image_plateau.bottomleft
-			coord_x -= 5
+			coord_x -= self.espace_entre_carte
 			coord_x -= self.default_hauteur_sprite
 			decalage_x = -(len(sprite_joueur_qui_joue[type_carte]) * (self.default_hauteur_sprite / 4))
 		else:
 			sprite_joueur_qui_joue = self.sprite_j2
 			coord_x, _ = self.rect_image_plateau.bottomright
-			coord_x += 5
+			coord_x += self.espace_entre_carte
 			decalage_x = len(sprite_joueur_qui_joue[type_carte]) * (self.default_hauteur_sprite / 4)
 		
 		if type_carte == 0:
@@ -264,12 +343,12 @@ class Fenetre:
 										
 										if sprit.carte in self.plateau.liste_cartes_prenables():
 												
-												self.__piocher_carte(sprit)
-												
-												for carte in self.plateau.liste_cartes_prenables():
-													carte.est_face_cachee = False
-			
-												self.plateau.joueur_qui_joue = self.plateau.obtenir_adversaire()
+											self.__piocher_carte(sprit)
+											
+											for carte in self.plateau.liste_cartes_prenables():
+												carte.est_face_cachee = False
+		
+											self.plateau.joueur_qui_joue = self.plateau.obtenir_adversaire()
 								
 			# PARTIE Update
 			if len(self.cartes_plateau) == 0 and self.plateau.age != 3:
@@ -282,17 +361,19 @@ class Fenetre:
 				group_sprit.update()
 			for group_sprit in self.sprite_j2:
 				group_sprit.update()
+			self.merverille_j1.update()
+			self.merverille_j2.update()
+			self.jetons_progres_plateau.update()
 				
 			# PARTIE Draw / render
 			self.ecran.blit(self.image_fond, (0, 0))
 			self.ecran.blit(
 				self.image_plateau,
-				(self.largeur / 2 - self.image_plateau.get_width() / 2,
-				2 * self.espace_entre_carte)
+				self.rect_image_plateau.topleft
 			)
-			self.rect_image_plateau.topleft = (
-				(self.largeur / 2 - self.image_plateau.get_width() / 2,
-				2 * self.espace_entre_carte)
+			self.ecran.blit(
+				self.image_banque,
+				self.rect_image_banque.topleft
 			)
 			
 			self.cartes_plateau.draw(self.ecran)
@@ -300,12 +381,18 @@ class Fenetre:
 				group_sprit.draw(self.ecran)
 			for group_sprit in self.sprite_j2:
 				group_sprit.draw(self.ecran)
+			self.merverille_j1.draw(self.ecran)
+			self.merverille_j2.draw(self.ecran)
+			self.jetons_progres_plateau.draw(self.ecran)
 			
+			# OUTIL DEBUG #
 			# pygame.draw.line(self.ecran, (255, 0, 0), (self.largeur/2, 0), (self.largeur/2, self.hauteur))
-			# pygame.draw.line(self.ecran, (255, 0, 0), self.rect_image_plateau.topleft,
-				# self.rect_image_plateau.bottomleft)
-			# pygame.draw.line(self.ecran, (255, 0, 0), self.rect_image_plateau.topright,
-				# self.rect_image_plateau.bottomright)
+			pygame.draw.line(self.ecran, (255, 0, 0), self.rect_image_plateau.topleft,
+				self.rect_image_plateau.bottomleft)
+			pygame.draw.line(self.ecran, (255, 0, 0), self.rect_image_plateau.topright,
+				self.rect_image_plateau.bottomright)
+			pygame.draw.line(self.ecran, (255, 0, 0), self.rect_image_plateau.bottomright,
+				self.rect_image_plateau.bottomleft)
 			
 			# after drawing everything, flip this display
 			pygame.display.flip()
--- src/interface_graphique/src/SpriteCarte.py
+++ src/interface_graphique/src/SpriteCarte.py
@@ -1,39 +1,21 @@
-from typing import Any
-
 import pygame
 
 from src.interface_graphique.src.Constantes import DOSSIER_IMAGES_CARTES
-from src.interface_graphique.src.Element import Element
+from src.interface_graphique.src.MonSprite import MonSprite
 from src.utils.Carte import Carte
 
 
-
-class SpriteCarte(Element):
+class SpriteCarte(MonSprite):
 	def __init__(self, carte: Carte, haut_gauche_x: float, haut_gauche_y: float, ration_longeur_fenetre: float):
-		super().__init__(haut_gauche_x, haut_gauche_y)
+		super().__init__(haut_gauche_x, haut_gauche_y, ration_longeur_fenetre)
 		
 		self.carte = carte
-		
-		self.ration_longeur_fenetre = ration_longeur_fenetre
-		self.angle = 0
 		
 		self.chemin_image_recto = None
 		self.chemin_image_verso = None
 		self.image = None
 		
 		self.preparer_image()
-	
-	def modifier_taille_image(self):
-		larg, haut = self.image.get_size()
-		ration_image = haut / larg
-		
-		if self.largeur_zoom == 0:
-			larg = larg * self.ration_longeur_fenetre
-		else:
-			larg = larg * self.largeur_zoom
-		
-		haut = ration_image * larg
-		self.image = pygame.transform.scale(self.image, (larg, haut))
 	
 	def charger_image(self):
 		self.chemin_image_recto = f"{DOSSIER_IMAGES_CARTES}{self.carte.nom}.jpg"
@@ -46,28 +28,4 @@ class SpriteCarte(Element):
 			self.image = pygame.image.load(self.chemin_image_recto).convert()
 		else:
 			self.image = pygame.image.load(self.chemin_image_verso).convert()
-			
-	def deplacer_image(self):
-		self.rect = self.image.get_rect()
 		
-		if self.largeur_zoom == 0:
-			self.rect.topleft = (self.haut_gauche_x, self.haut_gauche_y)
-		else:
-			self.rect.center = self.coord_milieu_fen
-			
-	def preparer_image(self):
-		self.charger_image()
-		self.modifier_taille_image()
-		self.deplacer_image()
-		
-	def pivoter(self):
-		self.image = pygame.transform.rotate(self.image, self.angle)
-		self.rect = self.image.get_rect(topleft=self.rect.bottomleft)
-	
-	def update(self, *args: Any, **kwargs: Any) -> None:
-		self.preparer_image()
-		
-		if self.angle != 0:
-			self.pivoter()
-			
-			
