7d1b96b6bba329019e60817fb75d3942f6a66a4f
m27bay <mickael.ledenmat@gmail.com>
2022-05-20 18:04:13 +0200

    Update main_test.py

--- main_test.py
+++ main_test.py
@@ -6,17 +6,20 @@ from src.interface_graphique.src.Fenetre import Fenetre
 from src.interface_graphique.src.SpriteCarte import SpriteCarte
 from src.utils.Carte import Carte
 from src.utils.Joueur import Joueur
+from src.utils.Merveille import Merveille
 from src.utils.Plateau import Plateau
 
 if __name__ == '__main__':
-	plateau = Plateau(Joueur("joueur"), Joueur("ordi"), False)
-	
 	# choix auto merveille
 	# plateau = Plateau(Joueur("joueur"), Joueur("ordi"))
+	plateau = Plateau(Joueur("joueur"), Joueur("ordi"), False)
+	
 	plateau.preparation_plateau()
 	
 	# facile = 5
 	# normal = 7
 	# difficile = 9
 	fenetre = Fenetre("7 wonder Duel", plateau, 5)
+	
 	fenetre.boucle_principale()
+	
